d32cc361e7bc11ad6b05e0476c17646f2cac6cc5
Benedikt Böhm <bb@xnull.de>
2013-03-05 19:23:23 +0100

    only set lxc attributes if no other plugin matched

--- dev-ruby/ohai/files/lxc_virtualization.rb
+++ dev-ruby/ohai/files/lxc_virtualization.rb
@@ -1,29 +1,31 @@
 provides "linux/lxc_virtualization"
 require_plugin "virtualization"
 
-if File.exists?("/proc/1/cgroup")
-  if File.read("/proc/1/cgroup").match(/^\d+:.+:\/$/) && File.exist?("/usr/sbin/lxc-start")
-    virtualization[:system] = "linux-lxc"
-    virtualization[:role] = "host"
-  elsif File.read("/proc/1/cgroup").match(/^\d+:.+:\/.+$/)
-    virtualization[:system] = "linux-lxc"
-    virtualization[:role] = "guest"
-  end
-end
-
-if virtualization[:system] == "linux-lxc" && virtualization[:role] == "host"
-  virtualization[:lxc] = Mash.new
-  virtualization[:lxc][:guests] = {}
-
-  lxc_guests = %x{lxc-ls}.split.uniq
-  lxc_running =  %x{lxc-ls --active}.split.uniq
-
-  lxc_guests.each do |guest|
-    virtualization[:lxc][:guests][guest] = {}
-    virtualization[:lxc][:guests][guest][:running] = false
+unless virtualization[:system]
+  if File.exists?("/proc/1/cgroup")
+    if File.read("/proc/1/cgroup").match(/^\d+:.+:\/$/) && File.exist?("/usr/sbin/lxc-start")
+      virtualization[:system] = "linux-lxc"
+      virtualization[:role] = "host"
+    elsif File.read("/proc/1/cgroup").match(/^\d+:.+:\/.+$/)
+      virtualization[:system] = "linux-lxc"
+      virtualization[:role] = "guest"
+    end
   end
 
-  lxc_running.each do |guest|
-    virtualization[:lxc][:guests][guest][:running] = true
+  if virtualization[:system] == "linux-lxc" && virtualization[:role] == "host"
+    virtualization[:lxc] = Mash.new
+    virtualization[:lxc][:guests] = {}
+
+    lxc_guests = %x{lxc-ls}.split.uniq
+    lxc_running =  %x{lxc-ls --active}.split.uniq
+
+    lxc_guests.each do |guest|
+      virtualization[:lxc][:guests][guest] = {}
+      virtualization[:lxc][:guests][guest][:running] = false
+    end
+
+    lxc_running.each do |guest|
+      virtualization[:lxc][:guests][guest][:running] = true
+    end
   end
 end
